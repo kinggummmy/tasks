c5a70d90f9e7b3d28e6a60515b97561729610662
Robert Costley <ncostley@udel.edu>
2022-03-12 19:57:23 -0500

    Three done

--- src/App.tsx
+++ src/App.tsx
@@ -11,63 +11,11 @@ import { Counter } from "./components/Counter";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header
-                style={{
-                    backgroundColor: "darkseagreen"
-                }}
-                className="App-header"
-            >
+            <header className="App-header">
                 <h1>This is a header.</h1>
-                <img
-                    src="https://i.imgur.com/mUMMMSv.png"
-                    alt="An image I used for an emote on Discord"
-                />
                 UD CISC275 with React Hooks and TypeScript<br></br>
                 Nick Costley<br></br>
                 Hello World<br></br>
-                <Button onClick={() => console.log("Hello World!")}>
-                    Log Hello World
-                </Button>
-                Three things I enjoy<br></br>
-                <ul>
-                    <li
-                        style={{
-                            border: "1px solid blue",
-                            padding: "4px",
-                            backgroundColor: "deepskyblue"
-                        }}
-                    >
-                        Mushrooms
-                    </li>
-                    <li style={{ border: "1px solid green", padding: "4px" }}>
-                        <b>Frogs</b>
-                    </li>
-                    <li style={{ border: "1px solid red", padding: "4px" }}>
-                        Root Beer
-                    </li>
-                </ul>
-                <Container>
-                    <Row>
-                        <Col>
-                            <div
-                                style={{
-                                    width: "300px",
-                                    height: "200px",
-                                    backgroundColor: "red"
-                                }}
-                            ></div>
-                        </Col>
-                        <Col>
-                            <div
-                                style={{
-                                    width: "300px",
-                                    height: "200px",
-                                    backgroundColor: "red"
-                                }}
-                            ></div>
-                        </Col>
-                    </Row>
-                </Container>
             </header>
             <hr></hr>
             <Counter></Counter>
--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,30 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [counter, setCounter] = useState<number>(4);
+    const [quizStarted, setQuiz] = useState<boolean>(false);
+    function subtractOne(): void {
+        setCounter(counter - 1);
+    }
+    function addOne(): void {
+        setCounter(counter - 1);
+    }
+    function startQuiz(): void {
+        counter > 0 ? subtractOne : counter;
+        !quizStarted;
+    }
+    return (
+        <>
+            <div>
+                <Button onClick={() => startQuiz()} disabled={quizStarted}>
+                    Start Quiz
+                </Button>
+                <Button onClick={() => setQuiz(false)} disabled={!quizStarted}>
+                    Stop Quiz
+                </Button>
+                <Button onClick={() => addOne()}> Mulligan </Button>
+                Attempts: {counter};
+            </div>
+        </>
+    );
 }
